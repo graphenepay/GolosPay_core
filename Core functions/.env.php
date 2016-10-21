70de340ce08d8f86d7704dc619312c986d607106
steempayments <walschot015@gmail.com>
2016-10-21 18:26:48 +0200

    First version
    Awaiting funds for testing.

---
+++ Core functions/.env.php
@@ -0,0 +1,12 @@
+<?php
+
+return array(
+
+    'ENABLE_DEBUG'    => false,
+    'GOLOSPAY_ACCOUNT' => 'YOUR_ACCOUNT',
+
+    'GOLOS_RPC_SERVER' => 'YOUR_IP',
+    'GOLOS_RPC_PORT' => YOUR_PORT,
+    'GOLOS_RPC_VERSION' => "2.0",
+
+);
---
+++ Core functions/classes/Payment.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * GolosPayments.com
+ * Payment object
+ */
+
+class Payment{
+
+    public  $payid, $to, $amount, $currency, $callback, $success;
+
+    function __construct($data)
+    {
+        if(array_has($data, "payid")) {$this->payid = $data['payid'];} else {$this->payid = GolosHelper::generatePaymentID();};
+
+        if(array_has($data, "callback")) {$this->callback = $data['callback'];} else {$this->callback = "http://www.Golospay.io";};
+        //
+        if (array_has($data, "amount") && array_has($data, "currency") && array_has($data, "receiver")) {
+
+            $this->amount = $data['amount'];
+            $this->currency = $data['currency'];
+            $this->to = $data['receiver'];
+
+
+            if ($this->amount != "0") {
+                // Fixed payment handler
+                $this->amount = number_format($this->amount, 3, '.', '');
+            }
+
+            $this->success = true;
+
+            // Create the data array
+
+            $data = array(
+                'success' => true,
+                'paymentID' => $this->payid,
+                'receiver' => $this->to,
+                'amount' => $this->amount,
+                'currency' => $this->currency,
+                'callback' => $this->callback,
+            );
+        } else
+        {
+            $this->success =false;
+
+            $data = array(
+                'success' => false,
+                'paymentID' => $this->payid,
+                'receiver' => $this->to,
+                'amount' => $this->amount,
+                'currency' => $this->currency,
+                'callback' => $this->callback,
+            );
+        }
+    }
+
+    function getPayment() {
+        return $this->payid;
+    }
+
+    function toArray()
+    {
+        return array(
+            array
+            (
+                'payid'=>$this->payid,
+                'to'=>$this->to,
+                'amount'=>$this->amount,
+                'callback'=>$this->callback,
+
+            )
+        );
+    }
+}
